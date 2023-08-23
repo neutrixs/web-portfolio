171f614a24f4de57b0907ba8f707f9ad47afa56d
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2023-08-23 20:43:43 +0700

    wheel and arrow keys listener

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -4,7 +4,36 @@ import styles from './style.module.scss'
 
 export default function MainPage() {
     const [currentSlide, setCurrentSlide] = useState(0)
-    const slides = useRef<React.ReactNode[]>([<Introduction key="introduction" />])
+    const slides = useRef<React.ReactNode[]>([
+        <Introduction key="introduction" />,
+        <div key="dick" />,
+    ])
+
+    useEffect(() => {
+        function wheel(event: WheelEvent) {
+            if (!event.deltaY) return
+            scroll(event.deltaY / Math.abs(event.deltaY))
+        }
+
+        function keydown(event: KeyboardEvent) {
+            switch (event.code) {
+                case 'ArrowUp':
+                    scroll(-1)
+                    break
+                case 'ArrowDown':
+                    scroll(1)
+                    break
+            }
+        }
+
+        document.addEventListener('wheel', wheel)
+        document.addEventListener('keydown', keydown)
+
+        return () => {
+            document.removeEventListener('wheel', wheel)
+            document.removeEventListener('keydown', keydown)
+        }
+    }, [])
 
     function scroll(inc: number) {
         setCurrentSlide((current) => {
