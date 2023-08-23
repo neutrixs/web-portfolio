ea5e289399fd5ef1264dcff93bdcba85da2a6373
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2023-08-23 19:20:35 +0700

    add scroll function

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Introduction from './introduction'
 import styles from './style.module.scss'
 
 export default function MainPage() {
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const slides = useRef<React.ReactNode[]>([<Introduction key="introduction" />])
+
+    function scroll(inc: number) {
+        setCurrentSlide((current) => {
+            const newSlidePos = current + inc
+            if (newSlidePos >= 0 && newSlidePos < slides.current.length) {
+                return newSlidePos
+            }
+            return current
+        })
+    }
+
     return (
         <div className={styles.fixedContainer}>
-            <div className={styles.slidesContainer}>
-                <Introduction />
+            <div
+                className={styles.slidesContainer}
+                style={{ transform: `translateY(${-currentSlide * 100}%)` }}
+            >
+                {slides.current}
             </div>
         </div>
     )
