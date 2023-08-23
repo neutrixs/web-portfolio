94f68d52c3a7b383b0ee08e6306b025d4f066565
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2023-08-23 21:19:51 +0700

    listen for scroll event

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,38 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Introduction from './introduction'
 import styles from './style.module.scss'
+import arrow from '../../icons/arrow.svg'
 
 export default function MainPage() {
+    const isTouchDevice = 'ontouchstart' in document.documentElement
     const [currentSlide, setCurrentSlide] = useState(0)
     const slides = useRef<React.ReactNode[]>([<Introduction key="introduction" />])
+
+    useEffect(() => {
+        function wheel(event: WheelEvent) {
+            if (!event.deltaY) return
+            scroll(event.deltaY / Math.abs(event.deltaY))
+        }
+
+        function keydown(event: KeyboardEvent) {
+            switch (event.code) {
+                case 'ArrowUp':
+                    scroll(-1)
+                    break
+                case 'ArrowDown':
+                    scroll(1)
+                    break
+            }
+        }
+
+        document.addEventListener('wheel', wheel)
+        document.addEventListener('keydown', keydown)
+
+        return () => {
+            document.removeEventListener('wheel', wheel)
+            document.removeEventListener('keydown', keydown)
+        }
+    }, [])
 
     function scroll(inc: number) {
         setCurrentSlide((current) => {
@@ -16,6 +44,19 @@ export default function MainPage() {
         })
     }
 
+    function scrollAccessibility() {
+        return (
+            <div className={styles.scrollAccessibility}>
+                <div onClick={() => scroll(-1)}>
+                    <img src={arrow} />
+                </div>
+                <div onClick={() => scroll(1)}>
+                    <img src={arrow} />
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className={styles.fixedContainer}>
             <div
@@ -24,6 +65,7 @@ export default function MainPage() {
             >
                 {slides.current}
             </div>
+            {isTouchDevice ? scrollAccessibility() : null}
         </div>
     )
 }
