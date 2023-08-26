326dffc83392347c43c536b2fc3176281050bccb
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2023-08-26 08:48:55 +0700

    fade in title

--- src/pages/main/projects/index.tsx
+++ src/pages/main/projects/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './style.module.scss'
 
 interface props {
@@ -7,9 +7,15 @@ interface props {
 }
 
 export default function Projects({ inView, height }: props) {
+    const [show, setShow] = useState(' ')
+
+    useEffect(() => {
+        setTimeout(() => setShow(' ' + (inView ? style.show : '')), 500)
+    }, [inView])
+
     return (
         <div className={style.container} style={{ height: height.toString() + 'px' }}>
-            <div></div>
+            <p className={style.title + show}>My Projects</p>
         </div>
     )
 }
