3849d3a2032a5df741c0260556b4b80a6880c208
neutrixs <ihsan.maulana0501@gmail.com>
2024-10-29 23:40:12 +0700

    title UI

--- src/pages/showcase/music/index.tsx
+++ src/pages/showcase/music/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import style from './music.module.scss'
+import coloraturaImg from '../../../img/coloratura.png'
 
 export default function Music() {
     return (
-        <div>
-            <div></div>
+        <div className={style.container}>
+            <div className={style.info}>
+                <img src={coloraturaImg} />
+                <div>
+                    <span>Coloratura</span>
+                    <span>Coldplay</span>
+                </div>
+            </div>
         </div>
     )
 }
