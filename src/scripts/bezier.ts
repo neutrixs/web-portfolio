c21c7ecffe2eab09a929986af6ec75011960188b
neutrixs <ihsan.maulana0501@gmail.com>
2024-11-02 20:39:44 +0700

    bezier function for future uses

---
+++ src/scripts/bezier.ts
@@ -0,0 +1,14 @@
+export interface Point {
+    x: number
+    y: number
+}
+
+export default function bezier(t: number, P1: Point, P2: Point) {
+    const x =
+        (1 - t) ** 3 * 0 + 3 * (1 - t) ** 2 * t * P1.x + 3 * (1 - t) * t ** 2 * P2.x + t ** 3 * 1
+
+    const y =
+        (1 - t) ** 3 * 0 + 3 * (1 - t) ** 2 * t * P1.y + 3 * (1 - t) * t ** 2 * P2.y + t ** 3 * 1
+
+    return { x, y }
+}
