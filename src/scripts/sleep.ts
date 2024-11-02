41dbdb654532a78bbb6f54d98e853c247b137daa
neutrixs <ihsan.maulana0501@gmail.com>
2024-11-02 21:19:48 +0700

    rename scripts to util

--- src/scripts/sleep.ts
+++
@@ -1,5 +0,0 @@
-export default function sleep(ms: number) {
-    return new Promise((a) => {
-        setTimeout(a, ms)
-    })
-}
--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -3,7 +3,7 @@ export interface Point {
     y: number
 }
 
-export default function bezier(t: number, P1: Point, P2: Point) {
+export function bezier(t: number, P1: Point, P2: Point) {
     const x =
         (1 - t) ** 3 * 0 + 3 * (1 - t) ** 2 * t * P1.x + 3 * (1 - t) * t ** 2 * P2.x + t ** 3 * 1
 
@@ -11,4 +11,10 @@ export default function bezier(t: number, P1: Point, P2: Point) {
         (1 - t) ** 3 * 0 + 3 * (1 - t) ** 2 * t * P1.y + 3 * (1 - t) * t ** 2 * P2.y + t ** 3 * 1
 
     return { x, y }
+}
+
+export function sleep(ms: number) {
+    return new Promise((a) => {
+        setTimeout(a, ms)
+    })
 }
